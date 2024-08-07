de230e4c8aeefe85d0e95a0d02dff19574d88ea5
Otb92 <taras.1592@gmail.com>
2024-08-07 15:03:07 +0300

    Added class Board

--- tictactoe.py
+++ tictactoe.py
@@ -1,2 +1,18 @@
 print("We fucking did it baby!")
-def board(self, player):
+
+class Board(object):
+	#has a list of tiles
+	def __init__(self):
+		self.tiles = range(9)
+
+	def arrange_board(self):
+		board = [self.tiles[0], self.tiles[1], self.tiles[2]], [self.tiles[3], self.tiles[4], self.tiles[5]], [self.tiles[6], self.tiles[7], self.tiles[8]]
+		return board
+
+	def print_board(self):
+		board = self.arrange_board()
+		for row in board:
+			sys.stdout.write("|")
+			for tile in row:
+				sys.stdout.write(str(tile) + "|")
+			sys.stdout.write("\n")
